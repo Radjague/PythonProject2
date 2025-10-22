16285789972151d68b569c494b8033fe76860afc
Radja <radjaguemoula@gmail.com>
2025-10-22 23:58:36 +0100

    refactoring

--- src/fizz_buzz.py
+++ src/fizz_buzz.py
@@ -1,23 +1,14 @@
 
 
 def fizz_buzz(n:int)-> str:
-  if n == 8:
-    return '8'
-  if n == 7:
-    return '7'
+
   if n==5:
     return 'buzz'
-
-  if n==4:
-    return '4'
 
   if n==3 or n==6 or n==9:
     return 'fizz'
 
-  if n==2:
-    return '2'
+  if n==0:
+   return 'fizzbuzz'
 
-  if n==1:
-    return '1'
-
-  return 'fizzbuzz'
+  return str(n)
